05e473d7bd86110575062730f5db698ee35c95c7
Miguel Fierro <emfierro2@gmail.com>
2021-12-21 14:35:17 -0800

    created working count function

---
+++ web_scraper/scraper.py
@@ -0,0 +1,30 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+# topic that will be scraping is Seattle
+URL = 'https://en.wikipedia.org/wiki/Seattle'
+results = requests.get(URL)
+
+# print(page.content)
+soup = BeautifulSoup(results.content, 'html.parser')
+
+# print(soup.prettify())
+
+
+report = []
+
+citations_needed = soup.find_all('sup', class_='noprint Inline-Template Template-Fact')
+
+def get_citations_needed_count():
+    count = 0
+    
+    for citation in citations_needed:
+        count += 1
+    print(count)
+    
+print('The amount of citations needed is')
+get_citations_needed_count()
+    
+
+
